a8aca32d562655780f8eee2f1fbd0b959b89fd85
nickscozza <85478138+nickscozza@users.noreply.github.com>
2023-12-17 18:07:06 +1100

    Update genetic_algorithm_test.py
    #IMPORTANT: Most of this code looks abnormal / strange due to experimentation with chatgpt. Therefore, disregard any code that does not make sense.
    #By 10am 18/12/23, it will be massively modifed to make sense. Thanks for your patience.

--- genetic_algorithm_test.py
+++ genetic_algorithm_test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#IMPORTANT: Most of this code looks abnormal / strange due to experimentation with chatgpt. Therefore, disregard any code that does not make sense.
+#By 10am 18/12/23, it will be massively modifed to make sense. 
+
 from genetic_algorithm import GeneticAlgorithm
 
 
